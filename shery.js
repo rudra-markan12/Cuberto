a8ca2918dc32e95adb478932b2fc0caec0268051
rudra95314 <rudramarkan0@gmail.com>
2024-08-14 14:23:50 +0530

    first commit

---
+++ shery.js
@@ -0,0 +1,13 @@
+Shery.mouseFollower({
+    //Parameters are optional.
+    skew: true,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+  });
+  mouseFollower();
+  Shery.makeMagnet(".magnet-target" /* Element to target.*/, {
+    //Parameters are optional.
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+  });
+  makeMagnet();
